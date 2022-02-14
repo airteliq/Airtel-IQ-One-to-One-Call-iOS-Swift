8710d49560072693a9b5e89c7a2d5804402bcb19
jaykumarvcx <57702957+jaykumarvcx@users.noreply.github.com>
2022-02-14 19:13:21 +0530

    fixed rest api issue

--- AirtelIQ/AirtelIQ/VCXHeader/VCXConstant.swift
+++ AirtelIQ/AirtelIQ/VCXHeader/VCXConstant.swift
@@ -13,7 +13,7 @@ import Foundation
     let kTry      = true
 
 /*Your webservice host URL, Keet the defined host when kTry = true */
-    let kBasedURL = "https://api.videoiq.airtel.in/"
+    let kBasedURL = "https://demo.videoiq.airtel.in/"
 
 
 /*The following information required, Only when kTry = true, When you hosted your own webservice remove these fileds*/
